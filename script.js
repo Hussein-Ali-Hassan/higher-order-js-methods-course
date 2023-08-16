b22c3307fdd8f0e6d1114166fc00e726d1259d24
Hussein Hassan <hussein.h.tech@gmail.com>
2023-08-16 22:15:30 +0300

    finish the project

--- script.js
+++ script.js
@@ -10,8 +10,13 @@ const wealthParent = document.getElementById("wealth");
 
 // Event listeners
 addUserBtn.addEventListener("click", addUser);
+doubleMoneyBtn.addEventListener("click", doubleMoney);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+sortBtn.addEventListener("click", sortRichest);
+calculateWealthBtn.addEventListener("click", calculateWealth);
 
-const users = [];
+// for saving the users globally in our app
+let users = [];
 
 // call function to get 3 users on load
 window.onload = () => {
@@ -26,31 +31,67 @@ async function addUser() {
     const data = await response.json();
 
     const user = data.results[0];
-    const firstName = user.name.first;
-    const lastName = user.name.last;
+    const fullName = `${user.name.first} ${user.name.last}`;
+    // const firstName = user.name.first;
+    // const lastName = user.name.last;
 
     const newUser = {
-      name: `${firstName} ${lastName}`,
+      name: fullName,
       money: Math.floor(Math.random() * 1_000_000),
     };
 
     users.push(newUser);
 
-    const userDiv = document.createElement("div");
-    userDiv.classList.add("flex", "justify-between");
-
-    userDiv.innerHTML = `<h3>${newUser.name}</h3>
-                         <span>${newUser.money}</span> `;
-    contentParent.appendChild(userDiv);
+    updateDom();
   } catch (error) {
     console.log(error);
   }
 }
 
-// [].filter
-// [].map
-// [].sort
-// [].reduce
+function doubleMoney() {
+  // map over the users array and double the money using map method
+  users = users.map((user) => {
+    return { name: user.name, money: user.money * 2 };
+  });
+
+  updateDom();
+}
+
+function showMillionaires() {
+  // filter the users array and return only the users with money more than 1 million using filter method
+  users = users.filter((user) => user.money > 1_000_000);
+
+  updateDom();
+}
+
+function sortRichest() {
+  // sort the users array by money using sort method
+  users = users.sort((a, b) => b.money - a.money);
+
+  updateDom();
+}
+
+function calculateWealth() {
+  // reduce the users array and return the total money using reduce method
+  const totalWealth = users.reduce((acc, current) => (acc += current.money), 0);
+
+  wealthParent.innerHTML = `<h3>Total Wealth:</h3> <strong>${totalWealth} $</strong>`;
+}
+
+function updateDom() {
+  // clear the content parent
+  contentParent.innerHTML = "";
+
+  users.forEach((user) => {
+    const userDiv = document.createElement("div");
+
+    userDiv.classList.add("flex", "justify-between");
+
+    userDiv.innerHTML = `<h3>${user.name}</h3> <span>${user.money} $</span> `;
+
+    contentParent.appendChild(userDiv);
+  });
+}
 
 // MATH METHODS
 // square root
@@ -65,3 +106,31 @@ Math.random(); // 0.123456789
 Math.floor(2.9); // 2
 // ceil number
 Math.ceil(2.1); // 3
+
+// normal filter vs arrow function higher-order method: filter
+
+const array = [4, 6, 3, 1, 5, 2, 7, 10, 9, 8];
+// let evenValues = [];
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] % 2 === 0) {
+//     evenValues.push(array[i]);
+//   }
+// }
+
+// const evenValues = array.filter((value) => value % 2 === 0);
+
+// normal sort vs arrow function higher-order method: sort
+let sortedByDescending = [];
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = i; j < array.length; j++) {
+    if (array[i] < array[j]) {
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+}
+
+sortedByDescending = array.sort((a, b) => b - a);
